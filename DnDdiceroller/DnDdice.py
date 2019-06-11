90d43ecd637973d98c706537b1ad514c8689254a
Fabia Mazzocchi <37086938+fineterra@users.noreply.github.com>
2019-06-11 13:21:07 +0100

    Update DnDdice.py
    Added the ability to choose a modifier. It also now shows the result for the single rolls as well as the total.

--- DnDdiceroller/DnDdice.py
+++ DnDdiceroller/DnDdice.py
@@ -1,12 +1,13 @@
 # Dice roller for DnD
 # Created by fineterra - https://github.com/fineterra
-
 import random
 
 # Ask user for type and number of dice
-print("Available dice: d4, d6, d8, d10, d12, d20, d100")
-die_type = input("What die do you want to roll?")
+print("\nAvailable dice: d4, d6, d8, d10, d12, d20, d100")
+die_type = input("\nWhat die do you want to roll? ")
 die_no = int(input("How many dice? "))
+die_mod = int(input("What's the modifier? "))
+print()
 
 # Roll dice
 roll = 0
@@ -14,42 +15,49 @@ roll = 0
 if die_type == "d4":
     while die_no > 0:
         die = random.randint(1, 4)
+        print("You rolled a", die)
         roll += die
         die_no -= 1
 
 elif die_type == "d6":
     while die_no > 0:
         die = random.randint(1, 6)
+        print("You rolled a", die)
         roll += die
         die_no -= 1
 
 elif die_type == "d8":
     while die_no > 0:
         die = random.randint(1, 8)
+        print("You rolled a", die)
         roll += die
         die_no -= 1
 
 elif die_type == "d10":
     while die_no > 0:
         die = random.randint(1, 10)
+        print("You rolled a", die)
         roll += die
         die_no -= 1
 
 elif die_type == "d12":
     while die_no > 0:
         die = random.randint(1, 12)
+        print("You rolled a", die)
         roll += die
         die_no -= 1
 
 elif die_type == "d20":
     while die_no > 0:
         die = random.randint(1, 20)
+        print("You rolled a", die)
         roll += die
         die_no -= 1
 
 elif die_type == "d100":
     while die_no > 0:
         die = random.randint(1, 100)
+        print("You rolled a", die)
         roll += die
         die_no -= 1
 
@@ -57,5 +65,7 @@ else:
     print("That die is not available")
     exit()
 
+roll += die_mod
+
 # Print results
-print("\nYou rolled a", roll)
+print("\nThe total result is", roll)
